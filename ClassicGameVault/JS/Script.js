6f32a35e43e1a8526e8467fbd127326f84cc7487
Matt Figueroa <79350397+MattJerson@users.noreply.github.com>
2024-01-14 23:22:59 +0800

    Update Script.js

--- ClassicGameVault/JS/Script.js
+++ ClassicGameVault/JS/Script.js
@@ -27,3 +27,37 @@ window.addEventListener("scroll", function () {
     }
   });
 });
+
+function sendMessage() {
+  // Get form values
+  var name = document.getElementById("name").value;
+  var userEmail = document.getElementById("user_email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  // Simple form validation
+  if (!name || !userEmail || !subject || !message) {
+    alert("Please fill out all the fields before sending the message.");
+    return;
+  }
+
+  // Prepare parameters for EmailJS
+  var params = {
+    from_name: name,
+    user_name: name,
+    user_email: userEmail,
+    subject_id: subject,
+    message: message,
+  };
+
+  // Send email using EmailJS
+  emailjs
+    .send("service_wqn5m7c", "template_2tjik8i", params)
+    .then(function (response) {
+      alert("Message Sent Successfully!");
+    })
+    .catch(function (error) {
+      console.error("Email sending failed", error);
+      alert("Email sending failed. Please try again later.");
+    });
+}
